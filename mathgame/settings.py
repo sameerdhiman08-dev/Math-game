540ccbaf0784e3bdc16e7f4cc9883b06d11f10b6
sameerdhiman08-dev <sameerdhiman08@gmail.com>
2025-09-21 14:42:22 +0530

    Production settings and collectstatic

--- mathgame/settings.py
+++ mathgame/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!=v@q$voxq)j$$vmy8lt+z7v9fn5ew5v+zc@!85lg)@^#o62b!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["mathgame.onrender.com"]
 
 
 
